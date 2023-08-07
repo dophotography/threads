1a5dfa6213d6168bf24b4722e34633a3ffec53d1
Dmytro Ohanov <dmitryohanovphotography@gmail.com>
2023-08-07 21:54:38 +0100

    init commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,42 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Topbar from "@/components/shared/Topbar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Threads",
+  description: "A Next.js 13 App Data",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          <Topbar />
+
+          <main>
+            <LeftSidebar />
+
+            <section className="main-container">
+              <div className="w-full max-w-4xl ">{children}</div>
+            </section>
+
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
